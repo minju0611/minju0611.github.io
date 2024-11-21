00149129bcb86d6eebdb7e879266fa213dfa1263
minju0611 <163393735+minju0611@users.noreply.github.com>
2024-11-21 16:57:31 +0900

    dddd

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732089802',resources: [
+    cacheName: 'chirpy-1732175582',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
